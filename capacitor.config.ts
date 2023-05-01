1bb18dd7b69a160350e250dbd9c59481b017be5b
William Fischer <william.fischer@ivvy.com>
2023-05-01 22:11:48 +1000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'eggless',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
